d4e3d30baceab30d03bb96e3d3a6926d19b50da9
Ryan Hemmila <ryanhemmila@gmail.com>
2021-09-09 20:19:12 -0500

    Duplicate symbol exception

---
+++ 1/DuplicateSymException.java
@@ -0,0 +1,6 @@
+import java.lang.Exception;
+
+/*
+ * Exception that indicates there are multiple of the same symbol in the symbol table
+ */
+public class DuplicateSymException extends Exception { }
