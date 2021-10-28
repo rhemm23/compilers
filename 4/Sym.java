2b1fb3acc131c8d5ecc5bdb93b6c89c015fce562
rhemm23 <ryanhemmila@gmail.com>
2021-10-28 15:10:39 -0500

    added p4

---
+++ 4/Sym.java
@@ -0,0 +1,15 @@
+public class Sym {
+    private String type;
+    
+    public Sym(String type) {
+        this.type = type;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public String toString() {
+        return type;
+    }
+}
